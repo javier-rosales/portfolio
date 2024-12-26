85c7033844e544cb008188f53aa61fbe0e260c54
Javier Rosales <cyjav.dev@gmail.com>
2024-12-26 15:25:53 -0600

    Add background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Background from './components/Background'
+
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl'>
-        Hola mundo!
-      </h1>
+      <Background />
     </div>
   )
 }
