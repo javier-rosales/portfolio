32383623bec2340f8457c88bd2c11abbdfb4cc4d
Javier Rosales <cyjav.dev@gmail.com>
2024-12-29 22:47:03 -0600

    Update header: sticky position and colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import ThemeSwitcher from './ThemeSwitcher'
 
 const Header = () => {
   return (
-    <header className='max-w-md mx-auto bg-[#0000000d] dark:bg-[#ffffff0f] backdrop-blur-sm shadow-md p-2 md:p-3 rounded-md'>
+    <header className='sticky top-2 border border-[#ebebeb]/80 dark:border-[#2e2e2e]/80 max-w-md mx-auto bg-[#ffffff]/80 dark:bg-[#0a0a0a]/80 backdrop-blur shadow-lg p-2 md:p-3 rounded-md'>
       <nav className='flex gap-3 md:gap-5 justify-center items-center text-sm'>
         <ul className='flex items-center gap-3 md:gap-5'>
           <li className='hover:text-[#0068d6] dark:hover:text-[#52a8ff]'>
