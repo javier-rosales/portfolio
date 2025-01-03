2de53d1d46a36e4b5c2f119f1dcf28b45a866eb7
Javier Rosales <cyjav.dev@gmail.com>
2025-01-02 22:00:01 -0600

    Fix: Header appears below education timeline dots and date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import ThemeSwitcher from './ThemeSwitcher'
 
 const Header = () => {
   return (
-    <header className='sticky top-2 border border-[#ebebeb]/80 dark:border-[#2e2e2e]/80 max-w-md mx-auto bg-[#ffffff]/80 dark:bg-[#0a0a0a]/80 backdrop-blur shadow-lg p-2 md:p-3 rounded-md'>
+    <header className='sticky z-10 top-2 border border-[#ebebeb]/80 dark:border-[#2e2e2e]/80 max-w-md mx-auto bg-[#ffffff]/80 dark:bg-[#0a0a0a]/80 backdrop-blur shadow-lg p-2 md:p-3 rounded-md'>
       <nav className='flex gap-3 md:gap-5 justify-center items-center text-sm'>
         <ul className='flex items-center gap-3 md:gap-5'>
           <li className='hover:text-[#0068d6] dark:hover:text-[#52a8ff]'>
