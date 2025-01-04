e93250d21d97d60c0bbe656c47df7c78fb6343d2
Javier Rosales <cyjav.dev@gmail.com>
2025-01-03 22:45:28 -0600

    Update space between project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,7 @@ const Projects = () => {
   return (
     <Section>
       <SectionTitle title='Proyectos' id='proyectos' />
-      <div className='flex flex-col gap-6 md:gap-8 lg:gap-10'>
+      <div className='flex flex-col gap-8 md:gap-10 lg:gap-12'>
         {projectList.map(({ id, ...project }) => (
           <ProjectCard
             key={id}
